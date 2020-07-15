3113f335ee7f68100e1fa26919b699113db7144a
Colin Whitcomb <colinsw@berkeley.edu>
2020-07-14 21:39:26 -0700

    Rearranged Inquirer prompts. Added buildTeam function. Synched up writeFile.

--- app.js
+++ app.js
@@ -1,109 +1,203 @@
-
-// const Manager = require("./lib/Manager");
-// const Engineer = require("./lib/Engineer");
-// const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-// const path = require("path");
+const path = require("path");
 const fs = require("fs");
+const Employee = require("./lib/Employee");
 
-// const OUTPUT_DIR = path.resolve(__dirname, "output");
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-// const render = require("./lib/htmlRenderer");
+const render = require("./lib/htmlRenderer");
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
+// array of employees to give to render
 const employees = [];
-console.log(employees);
+// console.log(employees);
+
+// inquirer prompts
+// var restart = function () {
 inquirer.prompt(
-    [{
-        // Prompt
-        type: "list",
-        name: "Prompt",
-        message: "Would you like to build an engineering team?",
-        choices: [
-            "Yes",
-            "No",
-        ]
-    },
-// !! create a function that will stop program if user chooses "No"
-    {
-        // Name
-        type: "input",
-        message: "What's their name?",
-        name: "name"
-    },
-    {
-        // ID
-        type: "input",
-        message: "What's their ID?",
-        name: "id"
-    },
-    {
-        // Email
-        type: "input",
-        message: "What's their email?",
-        name: "email"
-    },
-    {
-        // Role
-        type: "list",
-        message: "What is their role?",
-        name: "role",
-        choices: [
-          "Manager",
-          "Engineer",
-          "Intern"
-        ]
-      },
-// !! create a function that will ask specific questions determined on which role thay choose
+        [{
+                type: "list",
+                name: "prompt",
+                message: "Would you like to build an engineering team?",
+                choices: [
+                    "Yes",
+                    "No",
+                ]
+            },
+            // !! create a function that will stop program if user chooses "No"
+        ])
+    .then(data => {
+        // console.log(data);
+        if (data.prompt === 'Yes') {
+            askQuestions()
+        } else {}
+    });
 
-// --- Manager --- //
-      {
-        // Office Number
-        type: "input",
-        message: "What is their office number?",
-        name: "office_number"
-    },
+// ------Roll-------- // 
+var askQuestions = function () {
+    inquirer.prompt([{
 
-// --- Engineer --- //
-    {
-        // GitHub Username
-        type: "input",
-        message: "What is their GitHub username?",
-        name: "github"
-    },    
-
-// --- Intern --- //
-    {
-        // School
-        type: "School",
-        message: "What school do you attend?",
-        name: "school"
-    },    
-
-// --- Add another teammate? --- //
-{
-     // Add Another?
-     type: "list",
-     name: "Add",
-     message: "Would you like to build an engineering team?",
-     choices: [
-         "Yes",
-         "No",
-     ]
-}, 
-
-
-    ]).then(function (data) {
-        employees.push(data);
-      
+            type: "list",
+            message: "What is their role?",
+            name: "role",
+            choices: [
+                "Manager",
+                "Engineer",
+                "Intern"
+            ]
+        }, ])
+        .then(data => {
+            console.log("Employee data:");
+            // console.log(data);
+            if (data.role === 'Manager') {
+                askManager();
+            } else if (data.role === 'Engineer') {
+                askEngineer();
+            } else {
+                askIntern();
+            }
+        })
+};
 
 
 
+
+
+// ------Manager-------- // 
+var askManager = function () {
+    inquirer.prompt([{
+            // Name
+            type: "input",
+            message: "What's their name?",
+            name: "name"
+        },
+        {
+            // ID
+            type: "input",
+            message: "What's their ID?",
+            name: "id"
+        },
+        {
+            // Email
+            type: "input",
+            message: "What's their email?",
+            name: "email"
+        },
+        {
+            // Office Number
+            type: "input",
+            message: "What is their office number?",
+            name: "officeNumber"
+
+        },
+    ]).then(data => {
+        // console.log(data);
+        var manager = new Manager(data.name, data.id, data.email, data.officeNumber);
+        // console.log(manager);
+        employees.push(manager);
+        console.log(employees);
+        newTeamMember();
     })
+}
 
+
+// // --- Engineer --- //
+var askEngineer = function () {
+    inquirer.prompt([{
+            // Name
+            type: "input",
+            message: "What's their name?",
+            name: "name"
+        },
+        {
+            // ID
+            type: "input",
+            message: "What's their ID?",
+            name: "id"
+        },
+        {
+            // Email
+            type: "input",
+            message: "What's their email?",
+            name: "email"
+        },
+        {
+            // GitHub Username
+            type: "input",
+            message: "What is their GitHub username?",
+            name: "github"
+        },
+    ]).then(data => {
+        console.log(data);
+        var engineer = new Engineer(data.name, data.id, data.email, data.github);
+        employees.push(engineer);
+        newTeamMember();
+    })
+}
+
+// // --- Intern --- //
+var askIntern = function () {
+    inquirer.prompt([{
+            // Name
+            type: "input",
+            message: "What's their name?",
+            name: "name"
+        },
+        {
+            // ID
+            type: "input",
+            message: "What's their ID?",
+            name: "id"
+        },
+        {
+            // Email
+            type: "input",
+            message: "What's their email?",
+            name: "email"
+        },
+        {
+            // School
+            type: "input",
+            message: "What school do you attend?",
+            name: "school"
+        },
+    ]).then(data => {
+        var intern = new Intern(data.name, data.id, data.email, data.school);
+        employees.push(intern);
+        newTeamMember();
+    })
+}
+
+var newTeamMember = function () {
+    inquirer.prompt(
+            [{
+                    type: "list",
+                    name: "prompt",
+                    message: "Would you like to add another team member?",
+                    choices: [
+                        "Yes",
+                        "No",
+                    ]
+                },
+            ])
+        .then(data => {
+            // console.log(data);
+            if (data.prompt === 'Yes') {
+                askQuestions()
+            } else {
+                buildTeam();
+            }
+        });
+}
+
+var buildTeam = () => {
+// fs write and sync rendering of: 
+fs.writeFileSync('team.html', render(employees));
+
+};
 
 
 
@@ -120,9 +214,3 @@ inquirer.prompt(
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
